4b56cf0515ec3db3f472bea206d6cefc6b313666
Hamzah Ahmad <ahmadhamzah500@gmail.com>
2024-04-28 12:46:16 -0500

    feat: no posts found

--- app/components/PostsList.tsx
+++ app/components/PostsList.tsx
@@ -4,6 +4,13 @@ import PostCard from "./PostCard";
 import { PostWithDataType } from "../(pages)/posts/page";
 
 const PostList = ({ posts }: { posts: PostWithDataType[] }) => {
+  if (posts?.length < 1) {
+    return (
+      <div className="flex justify-center w-full h-32 items-center text-base-100 text-lg">
+        No Posts Found
+      </div>
+    );
+  }
   return (
     <div>
       {posts.map((post) => (
